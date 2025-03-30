039ffedcff04abae914493472448487f0ded95bb
tomsawyer0224 <binha0224@gmail.com>
2025-03-30 15:22:19 +0700

    test

--- test2.py
+++ test2.py
@@ -14,8 +14,8 @@ SYSTEM_MESSAGE = (
     "Please ensure that your responses are socially unbiased and positive in nature."
     "\n\nIf a question does not make any sense, or is not factually coherent, "
     "explain why instead of answering something not correct. "
-    "If you don't know the answer to a question, please use the searching tool, "
-    "please don't share false information."
+    "If you don't know the answer to a question, please use the tools, "
+    "don't share false information."
 )
 
 model = ChatOllama(model="llama3.2:1b", temperature = 0)
@@ -23,13 +23,14 @@ model = ChatOllama(model="llama3.2:1b", temperature = 0)
 search = DuckDuckGoSearchResults()
 tools = [search]
 
-# prompt = ChatPromptTemplate.from_messages(
-#     [
-#         ("system", SYSTEM_MESSAGE),
-#     ]
-# )
-graph = create_react_agent(model, tools)
-# graph = create_react_agent(model, tools, prompt=prompt)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", SYSTEM_MESSAGE),
+        ("human", "{question}")
+    ]
+)
+# graph = create_react_agent(model, tools)
+graph = create_react_agent(model, tools, prompt=prompt)
 
 
 # save_graph(graph, "abc.png")
@@ -55,10 +56,12 @@ graph = create_react_agent(model, tools)
 #         break
 
 # inputs = {"messages": [("user", "Who is Prime Minister of Canada?")]}
-inputs = {"messages": [("user", "Tell me about the earth quake in myanma")]}
-for s in graph.stream(inputs, stream_mode="values"):
-    message = s["messages"][-1]
-    if isinstance(message, tuple):
-        print(message)
-    else:
-       message.pretty_print()
+# inputs = {"messages": [("user", "Tell me about the earth quake in myanma")]}
+inputs = {"question": "Who is Prime Minister of Canada?"}
+graph.invoke(inputs)
+# for s in graph.stream(inputs, stream_mode="values"):
+#     message = s["messages"][-1]
+#     if isinstance(message, tuple):
+#         print(message)
+#     else:
+#        message.pretty_print()
