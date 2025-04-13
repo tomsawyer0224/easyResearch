cff606709eb900a1fe842cbb70fa176174c5a5a4
tomsawyer0224 <binha0224@gmail.com>
2025-04-13 16:51:01 +0700

    update code

--- app2.py
+++ app2.py
@@ -10,9 +10,6 @@ from typing import Any
 memory = MemorySaver()
 graph = builder.compile(checkpointer=memory)
 
-if "thread" not in st.session_state:
-    st.session_state.thread = None
-
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -63,8 +60,8 @@ async def generate_report(thread):
                 final_report = list(v.values())[0]
                 return final_report
 
-# thread = None    
-if topic:
+async def research(topic: str):
+    # plan
     st.chat_message("user").markdown(topic)
     st.session_state.messages.append({"role": "user", "content": topic})
     thread = {
@@ -78,29 +75,36 @@ if topic:
             "max_search_depth": 1,
         }
     }
-    st.session_state.thread = thread
     with st.spinner("Generating report plan...", show_time=True):
-        plan = asyncio.run(generate_plan(topic, thread))
+        # plan = asyncio.run(generate_plan(topic, thread))
+        original_plan = await generate_plan(topic, thread)
         with st.chat_message("assistant"):
-            st.markdown(plan)
-        st.session_state.messages.append({"role": "assistant", "content": plan})
-if feedback:
-    st.chat_message("user").markdown(feedback)
-    st.session_state.messages.append({"role": "user", "content": feedback})
-    thread = st.session_state.thread
-    st.write(f"thread: {thread}")
-    with st.spinner("Processing your feedback...", show_time=True):
-        plan = asyncio.run(process_feedback(feedback, thread))
-        with st.chat_message("assistant"):
-            st.markdown(plan)
-        st.session_state.messages.append({"role": "assistant", "content": plan})
+            st.markdown(original_plan)
+        st.session_state.messages.append({"role": "assistant", "content": original_plan})
 
-if st.session_state.final_report:
-    thread = st.session_state.thread
-    st.write(f"thread: {thread}")
-    with st.spinner("Generating the final report...", show_time=True):
-        final_report = asyncio.run(generate_report(thread))
-        with st.chat_message("assistant"):
-            st.markdown(final_report)
-        st.session_state.messages.append({"role": "assistant", "content": final_report})
-    st.session_state.final_report = False
+    # feedback
+    if feedback:
+        st.chat_message("user").markdown(feedback)
+        st.session_state.messages.append({"role": "user", "content": feedback})
+        with st.spinner("Processing your feedback...", show_time=True):
+            new_plan = await process_feedback(feedback, thread)
+            with st.chat_message("assistant"):
+                st.markdown(new_plan)
+            st.session_state.messages.append({"role": "assistant", "content": new_plan})
+
+    # final report
+    if st.session_state.final_report:
+        thread = st.session_state.thread
+        st.write(f"thread: {thread}")
+        with st.spinner("Generating the final report...", show_time=True):
+            final_report = await generate_report(thread)
+            with st.chat_message("assistant"):
+                st.markdown(final_report)
+            st.session_state.messages.append({"role": "assistant", "content": final_report})
+        st.session_state.final_report = False
+if topic not in st.session_state:
+    st.session_state.topic = None
+if topic:
+    st.session_state.topic = topic
+if st.session_state.topic is not None:
+    asyncio.run(research(st.session_state.topic))
--- test4.py
+++
@@ -1,7 +0,0 @@
-thread = None
-topic = 1
-print(thread)
-if topic:
-    thread = 1
-topic = 0
-print(thread)
